84ca3eafcd989f34aee57019ff62b6a95d1db05e
asmaabarj <asmaabarj5@gmail.com>
2025-01-13 09:51:48 +0100

    la configuration de l'environnement

---
+++ Back-end/src/test/java/com/redtrack/RedtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
