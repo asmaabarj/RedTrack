9ee1ccc7a1056518d2b5b5661a1bc113244fedf0
asmaabarj <asmaabarj5@gmail.com>
2025-02-14 09:39:03 +0100

    admin pré-enregistré

---
+++ Back-end/src/main/java/com/redtrack/config/AdminInitializer.java
@@ -0,0 +1,31 @@
+package com.redtrack.config;
+
+import com.redtrack.model.Role;
+import com.redtrack.model.User;
+import com.redtrack.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+        if (!userRepository.findByEmail("admin@redtrack.com").isPresent()) {
+            User admin = new User();
+            admin.setEmail("admin@redtrack.com");
+            admin.setPassword(passwordEncoder.encode("Admin123!"));
+            admin.setNom("Admin");
+            admin.setPrenom("Super");
+            admin.setRole(Role.ADMIN);
+            admin.setActive(true);
+            userRepository.save(admin);
+        }
+    }
+} 
